4977e9fcaee5fc35c7752b5e3d6e0d60a4a08714
Nicolas Chambrier <naholyr@gmail.com>
2015-01-26 00:07:28 +0100

    fix(fix fixing fix) but do it only if ?print-pdf detected, or it applies live too…

--- data/such-notes-print.js
+++ data/such-notes-print.js
@@ -5,36 +5,36 @@
     link.type = "text/css";
     link.href = "css/such-notes-print.css";
     document.getElementsByTagName("head")[0].appendChild(link);
-  }
 
-  var sections = document.querySelectorAll("section.slide");
-  var prev = null, prevHasNotes = false;
-  for (var j = 0; j < sections.length; j++) {
-    var section = sections[j];
+    var sections = document.querySelectorAll("section.slide");
+    var prev = null, prevHasNotes = false;
+    for (var j = 0; j < sections.length; j++) {
+      var section = sections[j];
 
-    // data-background not supported
-    if (section.getAttribute("data-background")) {
-      section.innerHTML = "<p>Slide background not included in print support</p>" + section.innerHTML;
-      section.removeAttribute("data-background");
-    }
-    if (section.getAttribute("data-background-video")) {
-      section.innerHTML = "<p>Video background not included in print support</p>" + section.innerHTML;
-      section.removeAttribute("data-background-video");
-    }
-    if (section.getAttribute("data-background-iframe")) {
-      section.innerHTML = "<p>Video background not included in print support</p>" + section.innerHTML;
-      section.removeAttribute("data-background-iframe");
-    }
+      // data-background not supported
+      if (section.getAttribute("data-background")) {
+        section.innerHTML = "<p>Slide background not included in print support</p>" + section.innerHTML;
+        section.removeAttribute("data-background");
+      }
+      if (section.getAttribute("data-background-video")) {
+        section.innerHTML = "<p>Video background not included in print support</p>" + section.innerHTML;
+        section.removeAttribute("data-background-video");
+      }
+      if (section.getAttribute("data-background-iframe")) {
+        section.innerHTML = "<p>Video background not included in print support</p>" + section.innerHTML;
+        section.removeAttribute("data-background-iframe");
+      }
 
-    if (!section.querySelector("aside.notes") && prev && !prev.querySelector("aside.notes")) {
-      // Two consecutive unannotated? Merge!
-      prev.classList.add("merge-next");
-      section.classList.add("merge-prev");
-      // Reset merge loop
-      prev = null;
-    } else {
-      // Just continue normal loop
-      prev = section;
+      if (!section.querySelector("aside.notes") && prev && !prev.querySelector("aside.notes")) {
+        // Two consecutive unannotated? Merge!
+        prev.classList.add("merge-next");
+        section.classList.add("merge-prev");
+        // Reset merge loop
+        prev = null;
+      } else {
+        // Just continue normal loop
+        prev = section;
+      }
     }
   }
 })();
