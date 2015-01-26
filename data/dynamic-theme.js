bb92fef2a6b724aa3d8467f5e701eaeb71a3c9bc
Nicolas Chambrier <naholyr@gmail.com>
2015-01-26 20:48:59 +0100

    feat(highlight) change highlight theme from query string ?higlight-theme=...

--- data/dynamic-theme.js
+++ data/dynamic-theme.js
@@ -4,4 +4,10 @@
   if (theme) {
     document.getElementById('theme').setAttribute('href', 'css/theme/' + theme + '.css');
   }
+
+  // Also change highlight.js theme!
+  var highlightTheme = Reveal.getQueryHash()['highlight-theme'];
+  if (highlightTheme) {
+    document.getElementById('highlight-theme').setAttribute('href', 'css/highlight/styles/' + highlightTheme + '.css');
+  }
 })();
